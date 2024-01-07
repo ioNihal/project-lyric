0ab180ddc43498b7cfd803fc356d61340d3749ff
PreDA <nihal04x@gmail.com>
2024-01-08 00:31:09 +0530

    multi songs

--- scripts/script.js
+++ scripts/script.js
@@ -14,10 +14,18 @@ var isPlaying = false;
 var sections = document.querySelectorAll('.page');
 var lyricSections = document.querySelectorAll('.lyric-con');
 var lyricCon = document.querySelector('.container');
+var sectionTimes = [];
+
+var s1 = document.getElementById('s1');
+var s2 = document.getElementById('s2');
+var s3 = document.getElementById('s3');
+var s4 = document.getElementById('s4');
+var s5 = document.getElementById('s5');
 
 var duration = document.getElementById('duration');
 var seektimer = 0;
 var durId;
+var totalDuration = 0;
 
 //var mainbtn = document.getElementById('button');
 var song1 = document.getElementById('song1');
@@ -31,15 +39,15 @@ var cardbtn = document.getElementById('play');
 });*/
 song1.addEventListener('click', () => {
     console.log("clicked song1");
-    startPlaying();
+    playRevenge();
 });
 song2.addEventListener('click', () => {
     console.log("clicked song2");
-    startPlaying();
+    playLetHerGo();
 });
 song3.addEventListener('click', () => {
     console.log("clickd  song3");
-    startPlaying();
+    playNoSong();
 });
 cardbtn.addEventListener('click', () => {
     console.log("clicked icon");
@@ -52,49 +60,65 @@ document.querySelector('.muteUnmuteButton').addEventListener('click', function (
 
 var mutebtn = document.getElementById('mute');
 mutebtn.addEventListener('click', function () {
-        if (audio.muted == true) {
-            audio.muted = false;
-            console.log('unmute');
-        }
-        else {
-            audio.muted = true;
-            console.log('mute');
-        }
-    });
+    if (audio.muted == true) {
+        audio.muted = false;
+        console.log('unmute');
+    }
+    else {
+        audio.muted = true;
+        console.log('mute');
+    }
+});
+
+function playRevenge() {
+    audio.src = 'songs/revenge.mp3';
+    console.log("Song set to Revenge.mp3");
+    totalDuration = audio.duration;
+    sectionTimes = [3, 6, 10, 15.5, 35.5];
+    console.log("Lyrics trigger point set");
+    s1.innerHTML = "Some kill, some steal, <br> some break your heart";
+    s2.innerHTML = "And you'd have thought that <br> i would let it go and let you walk";
+    s3.innerHTML = "Well, broken hearts break bones, <br> so in my grave, I'll rot";
+    s4.innerHTML = "And I don't wanna let it go, <br> so in my grave, I'll rot";
+    s5.innerHTML = "In my grave, I'll rot...";
+    console.log("Lyrics Changed");
+    startPlaying();
+}
+
+
 
 function startPlaying() {
     if (audio.paused) {
         sections[1].scrollIntoView({
             behavior: 'smooth'
         });
-        audio.currentTime = 68.9;
+        audio.currentTime = 0;
         audio.play();
         //durId = setInterval(timer, 1000);
         // debug for progress bar ::console.log("SongPlaying " + audio.duration + " & " + audio.currentTime + " and ");
         isPlaying = true;
-        const sectionTimes = [71.9, 74.9, 78.9, 84.4, 104.4];
         const progressBar = document.getElementById('seektime');
         let currentSectionIndex = 0;
         var lyriclen = 200;
-        var sdur = audio.duration;
+
 
         audio.addEventListener('timeupdate', () => {
-            if (audio.currentTime >= 78) {
-                duration.innerHTML = "0:" + (Math.floor(audio.currentTime) - 68);
+            if (audio.currentTime >= 10) {
+                duration.innerHTML = "0:" + Math.floor(audio.currentTime);
             }
             else
-                duration.innerHTML = "0:0" + (Math.floor(audio.currentTime) - 68);
+                duration.innerHTML = "0:0" + Math.floor(audio.currentTime);
 
-            let progressValue = (audio.currentTime / sdur) * 100;
+            let progressValue = (audio.currentTime / audio.duration) * 100;
             //for debug mode :: console.log("Progress Val : " + progressValue);
-            progressBar.value = progressValue - 56;
+            progressBar.value = progressValue;
             //for debug mode :: console.log("ProgressBAr Val : " + progressBar.value);
             if (audio.currentTime >= sectionTimes[currentSectionIndex]) {
                 lyricCon.scrollTop = lyriclen;
                 lyriclen += 200;
                 currentSectionIndex++;
             }
-            if (audio.currentTime > sectionTimes[4]) {
+            if ((audio.duration - audio.currentTime) == 0) {
                 clearInterval(durId);
                 duration.innerHTML = "0:00";
                 sections[2].scrollIntoView({
@@ -131,7 +155,7 @@ var observer = new IntersectionObserver(function (entries) {
     });
 });
 
-const elements = document.querySelectorAll('.soon');
+const elements = document.querySelectorAll('.lyrics-text');
 elements.forEach(function (element) {
     observer.observe(element);
 });
